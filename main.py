f0faa53afb25a4ef6243d38c7f919d8477f9cd06
petergpt <130558610+petergpt@users.noreply.github.com>
2023-10-29 20:39:36 +0000

    state issue fixed

--- main.py
+++ main.py
@@ -4,33 +4,35 @@ from api_logic import get_initial_tasks, get_refined_tasks, get_final_ideas
 def main():
     st.title("Optimize Your Job with AI")
 
-    # Step 1: User inputs their job description
-    job_description = st.text_area("Please describe what you do in your job:", "")
+    job_description = st.session_state.get('job_description', '')
+    user_feedback = st.session_state.get('user_feedback', '')
+    initial_tasks = st.session_state.get('initial_tasks', [])
+    refined_tasks = st.session_state.get('refined_tasks', [])
+
+    job_description = st.text_area("Please describe what you do in your job:", job_description)
+    st.session_state['job_description'] = job_description
+
     if st.button("Identify Tasks"):
-        with st.spinner('Identifying tasks...'):
-            try:
-                initial_tasks = get_initial_tasks(job_description)
-                st.subheader("Identified Tasks for Your Job")
-                for i, task in enumerate(initial_tasks, 1):
-                    st.write(f"{i}. {task}")
+        initial_tasks = get_initial_tasks(st.session_state['job_description'])
+        st.session_state['initial_tasks'] = initial_tasks
+        st.subheader("Suggested Tasks for Your Job")
+        for i, task in enumerate(initial_tasks, 1):
+            st.write(f"{i}. {task}")
 
-                # Step 3: User provides feedback
-                user_feedback = st.text_area("Provide feedback to amend the tasks:", "")
+        user_feedback = st.text_area("Provide your feedback on the tasks:", user_feedback)
+        st.session_state['user_feedback'] = user_feedback
 
-                if st.button("Generate Ideas"):
-                    with st.spinner('Generating ideas...'):
-                        try:
-                            # Step 4: Get refined set of tasks based on feedback
-                            refined_tasks = get_refined_tasks(job_description, initial_tasks, user_feedback)
+        if st.button("Refine Tasks"):
+            refined_tasks = get_refined_tasks(st.session_state['job_description'], initial_tasks, user_feedback)
+            st.session_state['refined_tasks'] = refined_tasks
+            st.subheader("Refined Tasks for Your Job")
+            for i, task in enumerate(refined_tasks, 1):
+                st.write(f"{i}. {task}")
 
-                            # Step 5: Get final set of ideas based on refined tasks
-                            final_ideas = get_final_ideas(refined_tasks)
-                            st.subheader("Top 10 Ideas for Your Job")
-                            st.markdown(final_ideas)
-                        except Exception as e:
-                            st.error(f"Something went wrong: {e}")
-            except Exception as e:
-                st.error(f"Something went wrong: {e}")
+            if st.button("Generate Ideas"):
+                final_ideas = get_final_ideas(refined_tasks)
+                st.subheader("Ideas for Applying Tasks")
+                st.markdown(final_ideas)
 
 if __name__ == "__main__":
     main()
