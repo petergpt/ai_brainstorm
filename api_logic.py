1617f46293d7f43a0be24626e7780d97ac81b99c
petergpt <130558610+petergpt@users.noreply.github.com>
2023-10-30 11:20:26 +0000

    theme and rate limit

--- api_logic.py
+++ api_logic.py
@@ -1,10 +1,15 @@
+from ratelimit import limits, sleep_and_retry
 from llm_capabilities import LLM_CAPABILITIES
 import openai
 import os
 
+ONE_HOUR = 3600  # One hour in seconds
+
 api_key = os.environ.get('OPENAI_API_KEY')
 openai.api_key = api_key
 
+@sleep_and_retry
+@limits(calls=20, period=ONE_HOUR)
 def get_initial_tasks(job_description):
     messages = [
         {"role": "system", "content": "You are a AI Brainstorm assistant. Before you identify ideas for AI, you need to identify the relevant tasks that a person does in their role. You are provided with a job description by the user. Identify the key tasks that this person typically does in their job, with the AI application in mind. Be succinct and clear"},
@@ -23,6 +28,8 @@ def get_initial_tasks(job_description):
       initial_tasks[0] = initial_tasks[0][3:]
     return initial_tasks
 
+@sleep_and_retry
+@limits(calls=20, period=ONE_HOUR)
 def get_final_ideas(initial_tasks):
     messages = [
         {"role": "system", "content": "You are an AI Brainstorm assistant. Given the tasks, identify what large language model capabilities from this list of pre-defined capabilities can help in defining ideas. Then come up with the Top 10 ideas based on these tasks, number each idea. Be succinct. Format in a markdown table with the following columns: #, Task, LLM Capability, Idea."},
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import streamlit as st
 from api_logic import get_initial_tasks, get_final_ideas
 
 def main():
-    st.title("Optimize Your Job with AI")
+    st.title("How can you apply LLMs to your job?")
 
     if 'job_description' not in st.session_state:
         st.session_state.job_description = ''
@@ -13,7 +13,7 @@ def main():
 
     job_description = st.text_area("Please describe what you do in your job:", st.session_state.job_description)
 
-    if st.button("Identify Tasks and Generate Ideas"):
+    if st.button("Identify Tasks and Generate AI Ideas"):
         with st.spinner('Step 1: Identifying tasks based on your job description...'):
             st.session_state.initial_tasks = get_initial_tasks(job_description)
 
@@ -21,7 +21,7 @@ def main():
         for i, task in enumerate(st.session_state.initial_tasks, 1):
             st.write(f"{i}. {task}")
 
-        with st.spinner('Step 2: Generating innovative ideas based on the identified tasks...'):
+        with st.spinner('Step 2: Generating AI ideas based on the identified tasks...'):
             st.session_state.final_ideas = get_final_ideas(st.session_state.initial_tasks)
 
         st.subheader("Ideas for Applying Tasks")
